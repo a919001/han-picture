5cbf889fd6c7ba4dd4cdd50df9866c2d8813fe5e
hanshan <1470397437@qq.com>
2025-02-26 00:36:48 +0800

    项目初始化 - 前端初始化 1. 全局通用布局 2. 路由配置 3. 全局请求配置 4. 全局状态管理

---
+++ han-picture-frontend/src/pages/HomePage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="homePage">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+const msg = "欢迎来到与山寒云图库，海量素材免费获取~";
+</script>
+
+<style scoped>
+#homePage {
+
+}
+</style>
