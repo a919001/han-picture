05be34047e494e9d0cc5499e73e0fb45e275ae87
hanshan <1470397437@qq.com>
2025-03-10 17:31:59 +0800

    图片模块 - 前端 1. 图片上传、创建、修改 2. 图片管理 3. 主页 4. 图片详情页 5. 图片下载

--- han-picture-frontend/src/router/index.ts
+++ han-picture-frontend/src/router/index.ts
@@ -2,7 +2,10 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from '@/pages/HomePage.vue'
 import UserLoginPage from '@/pages/user/UserLoginPage.vue'
 import UserRegisterPage from '@/pages/user/UserRegisterPage.vue'
-import userManagePage from '@/pages/admin/UserManagePage.vue'
+import UserManagePage from '@/pages/admin/UserManagePage.vue'
+import AddPicturePage from '@/pages/AddPicturePage.vue'
+import PictureManagePage from '@/pages/admin/PictureManagePage.vue'
+import PictureDetailPage from '@/pages/PictureDetailPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -25,7 +28,23 @@ const router = createRouter({
     {
       path: '/admin/userManage',
       name: '用户管理',
-      component: userManagePage,
+      component: UserManagePage,
+    },
+    {
+      path: '/admin/pictureManage',
+      name: '用户管理',
+      component: PictureManagePage,
+    },
+    {
+      path: '/add_picture',
+      name: '创建图片',
+      component: AddPicturePage,
+    },
+    {
+      path: '/picture/:id',
+      name: '图片详情',
+      component: PictureDetailPage,
+      props: true,
     },
   ],
 })
